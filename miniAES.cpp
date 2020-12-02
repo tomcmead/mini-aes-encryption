78f3d4da94527846bb91af9561c8303a36545fb1
Tom <tomcmead0@gmail.com>
2020-12-02 13:17:30 +0000

    Convert binary input into 4 nibbles

--- miniAES.cpp
+++ miniAES.cpp
@@ -1,9 +1,51 @@
+/*************************************************************************//**
+* @file miniAES.h
+* @brief Mini AES Controller
+*****************************************************************************/
+
+/*----------------------------------------------------------------------------
+  include files
+----------------------------------------------------------------------------*/
 #include <iostream>
+#include "miniAES.h"
 using namespace std;
+
+void dec2Binary(uint16_t dec, bool *binaryNum);
 
 int main(){
 
+    
+    uint16_t plaintext=0x9C63;      // 16-bit plaintext block
+
     // Transform 16 bit block into 4 nibbles
+    p_nibbles P;
+    bool binaryNum[16];
+    dec2Binary(plaintext, binaryNum);
+    //bool binaryNum[] = {0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1};
+    for(int i=0; i<4; i++){
+        P.p0[i] = binaryNum[i];
+        P.p1[i] = binaryNum[i+4];
+        P.p2[i] = binaryNum[i+8];
+        P.p3[i] = binaryNum[i+12];
+    }
+
+    cout << "Plaintext Bits:" << endl;
+    for(int i=0; i<4; i++){
+        cout << P.p0[i] << " ";
+    }
+    cout << " ";
+    for(int i=0; i<4; i++){
+        cout << P.p1[i] << " ";
+    }
+    cout << " ";
+    for(int i=0; i<4; i++){
+        cout << P.p2[i] << " ";
+    }
+    cout << " ";
+    for(int i=0; i<4; i++){
+        cout << P.p3[i] << " ";
+    }
+    cout << " "; 
 
 
 
@@ -32,4 +74,15 @@ int main(){
     // following round. 3 rounds in total for miniAES
 
     return 0;
+}
+
+void dec2Binary(uint16_t dec, bool *binaryNum){
+
+    int i = 0;
+    while(dec>0){
+        binaryNum[i] = dec % 2;
+        dec = dec / 2;
+        i++;
+    }
+    return;
 }
